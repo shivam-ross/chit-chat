2e6afa268a348a9df01549592e403382791cc282
Shivam Ross <144504100+shivam-ross@users.noreply.github.com>
2024-12-02 12:10:51 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  return {
-    define: {
-      __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    },
-  };
 })
